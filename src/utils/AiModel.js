215f3d2bec875df127da7a8d64a045b55bad764e
SubhamSaha9 <sahasubham7478@gmail.com>
2024-11-25 11:37:28 +0530

    gemini ai intigreated

---
+++ src/utils/AiModel.js
@@ -0,0 +1,27 @@
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+} from "@google/generative-ai";
+
+// const apiKey = process.env.GEMINI_API_KEY;
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash-exp",
+});
+
+const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+};
+
+export const aiChatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+});

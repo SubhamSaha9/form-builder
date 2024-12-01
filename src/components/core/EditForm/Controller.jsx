00ea200ef64aea80ab8460dc81111a80574753e5
SubhamSaha9 <sahasubham7478@gmail.com>
2024-12-01 22:21:57 +0530

    Theme Controller

--- src/components/core/EditForm/Controller.jsx
+++ src/components/core/EditForm/Controller.jsx
@@ -13,6 +13,38 @@ const Controller = ({ setSelectedTheme }) => {
     <div>
       {/* Theme selection controller */}
       <h2 className="my-1">Select Theme</h2>
+      <Select onValueChange={(value) => setSelectedTheme(value)}>
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          {themeData.map((theme, i) => (
+            <SelectItem value={theme.theme} key={i}>
+              <div className="flex gap-3">
+                <div className="flex">
+                  <div
+                    className="h-5 w-5 rounded-l-md"
+                    style={{ backgroundColor: theme.primary }}
+                  ></div>
+                  <div
+                    className="h-5 w-5"
+                    style={{ backgroundColor: theme.secondary }}
+                  ></div>
+                  <div
+                    className="h-5 w-5"
+                    style={{ backgroundColor: theme.accent }}
+                  ></div>
+                  <div
+                    className="h-5 w-5 rounded-r-md"
+                    style={{ backgroundColor: theme.neutral }}
+                  ></div>
+                </div>
+                {theme.theme}
+              </div>
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
     </div>
   );
 };
